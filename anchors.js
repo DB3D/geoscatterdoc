97bf5c057a0c5d7798fde292788fded5a8341851
DB3D <dodododorian@gmail.com>
2023-04-27 00:45:55 +0200

    small anchors modif
    no "h1" or "h2" in id
    and positional argument instead of named argument

--- anchors.js
+++ anchors.js
@@ -6,7 +6,8 @@ $(window).ready(function()
     elms.each(function()
     {
         let e = $(this);
-        let id = e.prop("tagName").toLowerCase() + '_' + e.text().toLowerCase().replace(/\W+/g, '_');
+        let idPrefix = e.prop("tagName").toLowerCase() === 'h1' ? 'section' : 'article';
+        let id = idPrefix + '_' + e.text().toLowerCase().replace(/\W+/g, '_');
         e.attr('id', id);
         e.attr('data-frag', id);
         let a = '<a href="#' + id + '" class="header-anchor">' + '<svg viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><path d="M568.2,644.1c-54.4,0-108.7-20.7-150.1-62.1c-11.2-11.2-11.2-29.5,0-40.8c11.2-11.2,29.5-11.2,40.8,0c60.3,60.3,158.4,60.3,218.7,0l209.6-209.6c60.3-60.3,60.3-158.4,0-218.7c-60.3-60.3-158.4-60.3-218.7,0L491.6,289.7c-11.2,11.2-29.5,11.2-40.8,0c-11.2-11.2-11.2-29.5,0-40.8L627.7,72.1c82.8-82.8,217.5-82.8,300.2,0c82.7,82.8,82.8,217.5,0,300.2L718.3,582C676.9,623.4,622.5,644.1,568.2,644.1L568.2,644.1L568.2,644.1z M222.2,990c-54.4,0-108.7-20.7-150.1-62.1c-82.8-82.8-82.8-217.5,0-300.2L281.7,418c82.8-82.8,217.5-82.8,300.2,0c11.2,11.2,11.2,29.5,0,40.8c-11.2,11.2-29.5,11.2-40.8,0c-60.3-60.3-158.4-60.3-218.7,0L112.9,668.4c-60.3,60.3-60.3,158.4,0,218.7c60.3,60.3,158.4,60.3,218.7,0l176.9-176.9c11.2-11.2,29.5-11.2,40.8,0c11.2,11.2,11.2,29.5,0,40.8L372.3,927.9C330.9,969.3,276.6,990,222.2,990L222.2,990L222.2,990z"></path></svg></a>';
@@ -33,7 +34,7 @@ $(window).ready(function()
     {
         event.preventDefault();
         // page frag in format expected in nav.js
-        top.window.location.hash = "#page=" + window.location.href.split('/').slice(-1)[0].split('.')[0] + "&article=" + $(this).parent().attr('id');
+        top.window.location.hash = "#" + window.location.href.split('/').slice(-1)[0].split('.')[0] + "&" + $(this).parent().attr('id');
         // scroll_to_header($(this).parent());
         scroll_to_header($(this).parent().attr('id'));
     });
@@ -110,47 +111,46 @@ function scrollToElement(id) {
     });
   });
 
+  function copyToClipboard(text) {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+  }
+
+  function showTemporaryMessage(message, duration) {
+    const msgElem = document.createElement('div');
+    msgElem.textContent = message;
+    msgElem.style.position = 'fixed';
+    msgElem.style.bottom = '20px';
+    msgElem.style.right = '20px';
+    msgElem.style.padding = '10px';
+    msgElem.style.backgroundColor = '#333';
+    msgElem.style.color = 'white';
+    msgElem.style.borderRadius = '5px';
+    document.body.appendChild(msgElem);
+
+    setTimeout(() => {
+      document.body.removeChild(msgElem);
+    }, duration);
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const headers = document.querySelectorAll('h1, h2');
+
+    headers.forEach(header => {
+      // Your existing header generation code...
+
+      anchor.addEventListener('click', (e) => {
+        e.preventDefault();
+        copyToClipboard(anchor.href);
+        showTemporaryMessage('Copied to clipboard', 2000);
+      });
+
+      header.appendChild(anchor);
+    });
+  });
+
 */
-
-//   function copyToClipboard(text) {
-//     const textarea = document.createElement('textarea');
-//     textarea.value = text;
-//     document.body.appendChild(textarea);
-//     textarea.select();
-//     document.execCommand('copy');
-//     document.body.removeChild(textarea);
-//   }
-
-//   function showTemporaryMessage(message, duration) {
-//     const msgElem = document.createElement('div');
-//     msgElem.textContent = message;
-//     msgElem.style.position = 'fixed';
-//     msgElem.style.bottom = '20px';
-//     msgElem.style.right = '20px';
-//     msgElem.style.padding = '10px';
-//     msgElem.style.backgroundColor = '#333';
-//     msgElem.style.color = 'white';
-//     msgElem.style.borderRadius = '5px';
-//     document.body.appendChild(msgElem);
-
-//     setTimeout(() => {
-//       document.body.removeChild(msgElem);
-//     }, duration);
-//   }
-
-//   document.addEventListener('DOMContentLoaded', function() {
-//     const headers = document.querySelectorAll('h1, h2');
-
-//     headers.forEach(header => {
-//       // Your existing header generation code...
-
-//       anchor.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         copyToClipboard(anchor.href);
-//         showTemporaryMessage('Copied to clipboard', 2000);
-//       });
-
-//       header.appendChild(anchor);
-//     });
-//   });
-
